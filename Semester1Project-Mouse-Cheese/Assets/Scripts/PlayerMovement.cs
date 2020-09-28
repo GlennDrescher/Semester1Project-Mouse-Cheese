44f62a416c59d10fa616f89e1f71715176925336
Glenn Drescher <GlennD@me.com>
2020-09-28 11:55:44 +0200

    #4 Created Input Axis and movement script for player 1

--- Semester1Project-Mouse-Cheese/Assets/Scripts/PlayerMovement.cs
+++ Semester1Project-Mouse-Cheese/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float movementSpeed = 5f;
+    
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,17 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+        float p1_MoveHorizontal = Input.GetAxis("P1_Horizontal");
+        float p1_MoveVertical = Input.GetAxis("P1_Vertical");
+        float p2_MoveHorizontal = Input.GetAxis("P2_Horizontal");
+        float p2_MoveVertical = Input.GetAxis("P2_Vertical");
+        
+        Vector3 p1_movement = new Vector3(p1_MoveHorizontal, p1_MoveVertical, 0f);
+        
+        transform.Translate(p1_movement * Time.deltaTime * movementSpeed);
+        
     }
+    
+    
+   
 }
