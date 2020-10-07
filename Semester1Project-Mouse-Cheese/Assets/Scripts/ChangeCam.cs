114f6461c217d25fc6883ffec7eef5d1b217ced5
Oliver Bresson <olbre20@student.sdu.dk>
2020-10-07 09:53:43 +0200

    #8 Made some small changes so it works a little smoother

--- Semester1Project-Mouse-Cheese/Assets/Scripts/ChangeCam.cs
+++ Semester1Project-Mouse-Cheese/Assets/Scripts/ChangeCam.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ChangeCam : MonoBehaviour
 {
-    public GameObject objectToCheck;
+    public GameObject objectToCheck1;
+    public GameObject objectToCheck2;
     public float distanceToObject = 15f;
     public Camera playerCam1;
     public Camera playerCam2;
@@ -19,14 +20,14 @@ public class ChangeCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, objectToCheck.transform.position) <= distanceToObject)
+        if (Vector3.Distance(objectToCheck1.transform.position, objectToCheck2.transform.position) <= distanceToObject)
         {
             playerCam1.enabled = false; 
             playerCam2.enabled = false;
             allPlayerCam.enabled = true;
         }
 
-        if (Vector3.Distance(transform.position, objectToCheck.transform.position) >= distanceToObject)
+        if (Vector3.Distance(objectToCheck1.transform.position, objectToCheck2.transform.position) >= distanceToObject)
         {
             playerCam1.enabled = true;
             playerCam2.enabled = true;
