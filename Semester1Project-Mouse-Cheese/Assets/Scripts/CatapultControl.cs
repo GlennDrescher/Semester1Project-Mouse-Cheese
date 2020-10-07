9d6c2b2c84f0f6326584e7ae5ecc4aa0b9ec1943
Unknown <unknown@example.com>
2020-10-07 11:12:00 +0200

    Temp debug items + coming catapult control script

---
+++ Semester1Project-Mouse-Cheese/Assets/Scripts/CatapultControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CatapultControl : MonoBehaviour
+
+{
+    private bool colcheck;
+    private GameObject catapult;
+    private Vector2 mountpos;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        catapult = GameObject.Find("catapult");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("djdjd");
+        if (collision.gameObject.tag.Equals("catapult") == true)
+        {
+            Debug.Log("Virker det her??");
+            if (gameObject.tag.Equals("Cheese"))
+            {
+
+                mountpos = GameObject.FindGameObjectWithTag("Mounts").transform.position;
+                gameObject.transform.position = new Vector2(mountpos.x,mountpos.y);
+                Debug.Log("Test");
+            }
+            else
+            {
+                Debug.Log("Det virker IKKE... :(");
+            }
+        }
+    }
+
+
+}
