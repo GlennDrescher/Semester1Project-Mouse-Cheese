19f24bf26aae50a6f3bd55bf1bcb4c9656434afe
Glenn Drescher <GlennD@me.com>
2020-12-22 04:51:55 +0100

    Add Pickup_Minimap

---
+++ Semester1Project-Mouse-Cheese/Assets/Scripts/Pickup_ActivateMinimap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup_ActivateMinimap : MonoBehaviour
+{
+
+    private Canvas minimap = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minimap = GameObject.Find("Minimap").GetComponent<Canvas>();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<ParticleSystem>().Play();
+        GetComponent<SpriteRenderer>().enabled = false;
+        StartCoroutine(ActivateMinimapForXseconds(5f));
+    }
+
+    private IEnumerator ActivateMinimapForXseconds(float x)
+    {
+        minimap.enabled = true;
+        yield return new WaitForSeconds(x);
+        minimap.enabled = false;
+        Destroy(gameObject);
+    }
+}
