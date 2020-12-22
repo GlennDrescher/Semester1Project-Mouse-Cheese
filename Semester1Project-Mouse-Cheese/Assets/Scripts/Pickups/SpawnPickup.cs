f0c1eda0f60d4e1188c02c514ad95f7bfc5a6310
Glenn Drescher <GlennD@me.com>
2020-12-22 05:50:31 +0100

    Create PickupSpawner

---
+++ Semester1Project-Mouse-Cheese/Assets/Scripts/Pickups/SpawnPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SpawnPickup : MonoBehaviour
+{
+
+    private GameObject[] pickups;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickups = Resources.LoadAll("Spawnables/Pickups", typeof(GameObject)).Cast<GameObject>().ToArray<GameObject>();
+        Instantiate(pickups[Random.Range(0, pickups.Length)], transform.position, transform.rotation);
+    }
+}
